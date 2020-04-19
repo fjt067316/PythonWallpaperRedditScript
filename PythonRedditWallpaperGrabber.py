2e12da1d80e8a79b1085bf689897b04e79db44ce
fjt067316 <63973505+fjt067316@users.noreply.github.com>
2020-04-19 15:07:43 -0300

    Add files via upload

---
+++ PythonRedditWallpaperGrabber.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+
+headers = {'user-agent': '/u/proturtle46\'s python bot' }
+
+r = requests.get('https://www.reddit.com/r/wallpaper/hot.json?limit=1000', headers = headers)
+
+#Turn website into text html code
+
+html = BeautifulSoup(r.content, 'html.parser')
+
+# " https://i.redd.it/ .... " put links to wallpaper in array
+
+links = re.findall(r'\bhttps://i.redd.it/\w+.jpg' , html.text)
+print(links)
+
+#put all wallpapers in folder
+
+i=0
+for i in range(len(links)):
+    jpg = requests.get(links[i])
+    name = re.findall(r'\bcomments/.....' , html.text)
+    path = 'C:/Users/Josh/Desktop/wallpapers/{}.jpg'.format(name[i][-5:])
+    i += 1
+    with open( path, 'wb') as f:
+        f.write(jpg.content)
+
+else:
+    exit()
+
+
+
+
+
